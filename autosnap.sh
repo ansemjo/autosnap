c3195edcf59b06bcd9d086c3ab6487763a7068ab
Anton Semjonov <anton@semjonov.de>
2019-04-08 19:37:44 +0200

    tidy up and improve comments
    had a little detour into preparing lists of snapshots to create and
    passing it as a list to a single zfs invocation but that complicated the
    script. while it might be more performant and would create atmoic
    snapshots, I opted for the KISS principle .. keep it simple.
    
    think about proper atomic snapshots again when zfs 8.0 and channel
    programs are available.

--- autosnap.sh
+++ autosnap.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
+# Create scheduled zfs snapshots and purge old snapshots
+# following simple x-in-timeframe retention policies.
+
 # generate a list of prefixed properties
+#  - ansemjo:autosnap (yes/no): perform automatic snapshots on this dataset
+#  - ansemjo:autosnap:keep_* (int): configure retention policies of old snapshots
 props() { echo ansemjo:autosnap{,:keep_{minimum,minutes,hours,days,weeks,months,years}} | tr ' ' ','; }
 
 # get a recursive list of datasets
 list() { zfs list -Hp -r -t filesystem,volume -s name -o name,$(props) "$@"; }
 listheader() { printf 'name\tautosnap\tminimum\tminutes\thours\tdays\tweeks\tmonths\tyears\n'; }
 
-# get a list of applicable snapshots
+# get a list of applicable snapshots per dataset
 snaplist() { zfs list -Hp -d1 -t snapshot -o name,creation "$1" | grep "$1@autosnap:" ; }
-
-# datesieve alias to apply needed resub
-sieve() { datesieve --resub '.*\t' '' --strptime '%s' --sort "$@"; }
 
 # apply retention policies from properties or use defaults
 retention() {
@@ -27,22 +29,27 @@ retention() {
 
 # ---------------------------------------
 
-if [[ -z $1 ]]; then
-  echo "usage: $0 pool/dataset [...]" >&2
-  exit 1
-fi
+# show a nice overview for debugging
+(listheader; list "$@") | column -t
 
+# common execution timestamp
 timestamp=$(date --utc +%FT%T%Z)
 
-(listheader; list "$@") | column -t
+# iterate over datasets
 list "$@" | while IFS=$'\t' read name autosnap minimum minutes hours days weeks months years; do
 
-  # check if we should operate on this at all
-  if [[ $autosnap = no ]]; then
+  # check if we should operate on this dataset at all ('no' or anything else than inherited/'yes' --> skip)
+  # this check is overdefined but makes the intention clearer
+  if [[ $autosnap = no ]] || ! ( [[ $autosnap = - ]] || [[ $autosnap = yes ]] ); then
     echo "skip $name" >&2
     continue
   fi
   
+  # create new snapshot
+  newsnap="$name@autosnap:$timestamp"
+  zfs snapshot "$newsnap"
+  echo "created snapshot: $newsnap"
+
   # apply default retention policies
   minimum=$(retention "$minimum"  3 "$name" minimum)
   minutes=$(retention "$minutes"  1 "$name" minutes)
@@ -51,11 +58,10 @@ list "$@" | while IFS=$'\t' read name autosnap minimum minutes hours days weeks 
     weeks=$(retention "$weeks"    4 "$name" weeks)
    months=$(retention "$months"  12 "$name" months)
     years=$(retention "$years"   10 "$name" years)
-  
-  zfs snapshot "$name@autosnap:$timestamp"
-  echo "created $name@autosnap:$timestamp"
 
-  snaplist "$name" | sieve \
+  # sieve old snapshots and purge
+  snaplist "$name" | datesieve --sort \
+    --resub '.*\t' '' --strptime '%s' \
     --minimum "$minimum" \
     --minutes "$minutes" \
     --hours "$hours" \
